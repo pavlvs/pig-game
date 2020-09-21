5230f04bd2288baca053a117dac57eac8d90029a
Paul J. Shaw <pavlvsxavier@gmail.com>
2020-09-21 11:35:18 +0100

    added game initialization function and state variable

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
-// Constructor
+/*
+** DOM selection
+*/
 
 const buttonRoll = document.querySelector('.btn-roll')
 const buttonHold = document.querySelector('.btn-hold')
+const buttonNew = document.querySelector('.btn-new')
 const dice1 = document.getElementById('dice-1')
 // const dice2 = document.getElementById('dice-2')
 const player1Score = document.getElementById('score-0')
@@ -10,61 +13,66 @@ const player2Score = document.getElementById('score-1')
 const player2Current = document.getElementById('current-1')
 const player1Panel = document.querySelector('.player-0-panel')
 const player2Panel = document.querySelector('.player-1-panel')
+const player1Name = document.getElementById('name-0')
+const player2Name = document.getElementById('name-1')
 
-let scores, roundScore, activePlayer
+let scores, roundScore, activePlayer, gameOn
 
-scores = [0, 0]
-roundScore = 0
-activePlayer = 0
+init()
 
-player1Score.textContent = '0';
-player2Score.textContent = '0';
-player1Current.textContent = '0';
-player2Current.textContent = '0';
+/*
+** Events
+*/
 
-dice1.style.display = 'none'
-// dice2.style.display = 'none'
-
-// Events
 buttonRoll.addEventListener('click', function () {
-    let dice = Math.floor(Math.random() * 6) + 1
-    dice1.style.display = 'block'
-    dice1.src = '/assets/images/dice-' + dice + '.png'
+    if (gameOn) {
+        let dice = Math.floor(Math.random() * 6) + 1
+        dice1.style.display = 'block'
+        dice1.src = '/assets/images/dice-' + dice + '.png'
 
-    if (dice !== 1) {
-        roundScore += dice
-        activePlayer == 0
-            ? player1Current.textContent = roundScore
-            : player2Current.textContent = roundScore
-    } else {
-        nextPlayer()
+        if (dice !== 1) {
+            roundScore += dice
+            activePlayer == 0
+                ? player1Current.textContent = roundScore
+                : player2Current.textContent = roundScore
+        } else {
+            nextPlayer()
+        }
     }
 })
 
 buttonHold.addEventListener('click', function () {
-    scores[activePlayer] += roundScore
-    activePlayer == 0
-        ? player1Score.textContent = scores[activePlayer]
-        : player2Score.textContent = scores[activePlayer]
+    if (gameOn) {
+        scores[activePlayer] += roundScore
+        activePlayer == 0
+            ? player1Score.textContent = scores[activePlayer]
+            : player2Score.textContent = scores[activePlayer]
 
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
-        if (activePlayer == 0) {
-            player1Panel.classList.add('winner')
-            player1Panel.classList.remove('active')
+        if (scores[activePlayer] >= 100) {
+            gameOn = false
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
+            if (activePlayer == 0) {
+                player1Panel.classList.add('winner')
+                player1Panel.classList.remove('active')
+            } else {
+                player2Panel.classList.add('winner')
+                player2Panel.classList.remove('active')
+            }
+
+            dice1.style.display = 'none'
         } else {
-            player2Panel.classList.add('winner')
-            player2Panel.classList.remove('active')
+            nextPlayer()
+
         }
-
-        dice1.style.display = 'none'
-    } else {
-        nextPlayer()
     }
-
 })
 
-// Functions
+buttonNew.addEventListener('click', init)
+
+/*
+** Functions
+*/
+
 function nextPlayer() {
     activePlayer === 0
         ? activePlayer = 1
@@ -79,6 +87,29 @@ function nextPlayer() {
     player2Panel.classList.toggle('active')
 
     dice1.style.display = 'none'
+}
+
+function init() {
+    gameOn = true
+    scores = [0, 0]
+    roundScore = 0
+    activePlayer = 0
+
+    player1Score.textContent = '0';
+    player2Score.textContent = '0';
+    player1Current.textContent = '0';
+    player2Current.textContent = '0';
+    player1Name.textContent = 'Player 1';
+    player2Name.textContent = 'Player 2';
+
+    player1Panel.classList.remove('winner')
+    player1Panel.classList.remove('winner')
+    player1Panel.classList.remove('active')
+    player2Panel.classList.remove('active')
+    player1Panel.classList.add('active')
+
+    dice1.style.display = 'none'
+    // dice2.style.display = 'none'
 }
 
 
